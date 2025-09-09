4e600c9ede906a4e809d36b87fb9a8d71a5b4e0d
MurMur <87148235+Muriilow@users.noreply.github.com>
2025-09-08 21:34:58 -0300

    Updating neovim setup

--- nvim/lua/muriilo/plugins/theme.lua
+++ nvim/lua/muriilo/plugins/theme.lua
@@ -9,16 +9,47 @@ return {
             keywordStyle = { italic = true},
             statementStyle = { bold = true },
             typeStyle = {},
-            transparent = true,         -- do not set background color
             dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-            overrides = function(colors) -- add/modify highlights
-                return {}
-            end,
             theme = "dragon",              -- Load "wave" theme when 'background' option is not set
             background = {               -- map the value of 'background' option to a theme
                 dark = "dragon",           -- try "dragon" !
                 light = "lotus"
             },
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none"
+                        }
+                    }
+                }
+            },
+            overrides = function(colors)
+                local theme = colors.theme
+                local makeDiagnosticColor = function(color)
+                    local c = require("kanagawa.lib.color")
+                    return { fg = color, bg = c(color):blend(theme.ui.bg, 0.95):to_hex() }
+                end
+
+                return {
+                    NvimTreeNormal = { bg = theme.ui.bg_p1},
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                    Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
+                    PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                    PmenuSbar = { bg = theme.ui.bg_m1 },
+                    PmenuThumb = { bg = theme.ui.bg_p2 },
+                    DiagnosticVirtualTextHint  = makeDiagnosticColor(theme.diag.hint),
+                    DiagnosticVirtualTextInfo  = makeDiagnosticColor(theme.diag.info),
+                    DiagnosticVirtualTextWarn  = makeDiagnosticColor(theme.diag.warning),
+                    DiagnosticVirtualTextError = makeDiagnosticColor(theme.diag.error),
+                }
+            end,
         })
     end,
 }
--- nvim/lua/muriilo/plugins/treesitter.lua
+++ nvim/lua/muriilo/plugins/treesitter.lua
@@ -5,4 +5,51 @@ return {
         "nvim-treesitter/nvim-treesitter-textobjects", -- Opcional: para textobjects
         "windwp/nvim-ts-autotag",                     -- Opcional: autofechamento de tags HTML/XML
     },
+    config = function() 
+        local treeSitter = require("nvim-treesitter")
+
+        treeSitter.setup({
+            -- Lista de parsers para instalar automaticamente
+            ensure_installed = {
+                "c", "cpp", "lua", "python", "javascript", "typescript",
+                "html", "css", "json", "bash", "markdown", "vim", "vimdoc"
+            },
+
+            -- Ativar highlight
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+
+            -- Ativar indentação (experimental)
+            indent = {
+                enable = true,
+            },
+
+            -- Configurações extras (opcionais)
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                },
+            },
+
+            -- Textobjects (requer o plugin de dependencies)
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
+                },
+            },
+        })
+    end,
 }
